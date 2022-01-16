c2f2bc89761d5f1515937e26f9848a30b8098dfb
Ivan Cherkas <distor6on@yandex.ru>
2022-01-16 11:30:53 +0300

    finishing server

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import { TodoModule } from './modules/todo/todo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), TodoModule],
--- src/modules/todo/todo.module.ts
+++ src/modules/todo/todo.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TodoController } from './controllers/todo.controller';
 import { Todo } from './entities/todo.entity';
+import { TodoController } from './controllers/todo.controller';
 import { TodoService } from './services/todo.service';
 
 @Module({
